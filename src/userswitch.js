2f75b8b9887a09b0777c7d954af7ed01b08623f4
mhaks <mike.hakanson@gmail.com>
2024-02-08 16:56:29 -0700

    Adding user switch functionality

--- src/userswitch.js
+++ src/userswitch.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { Form, } from "react-router-dom";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 export default function UserSwitch() {
 
     const [users, setUsers] = useState([]);
+    const[selectedUser, setSelectedUser] = useState();
 
     useEffect(() => {
         const url = API_URL + "/CommerceDemo/Users";
@@ -15,14 +17,37 @@ export default function UserSwitch() {
     }, []);
 
     const userTemplate = users.map((item, index) => (
-        <option value={item.id} key={item.id}>{item.userName}</option>
+        <option value={item.userName} key={item.id} >{item.userName} </option>
     ));
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        console.log(formData);
+        console.log(formData.get("userName"));
+        const url = API_URL + "/CommerceDemo/ChangeUser";
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: formData.get("userName")
+        });
+        if (response.ok) {
+            window.location.reload();
+        } else {
+            console.error("Error switching user");
+        }
+    }
+
+  
 
     return(
         <div>
-            <form  method="post" >
+            <form method="post" onSubmit={handleSubmit} >
                 <div className="input-group">
-                    <select className="form-control" name="name">
+                    <select className="form-control" name="userName" >
                         {userTemplate}
                     </select>
                     <button type="submit" className="btn btn-outline-light">Switch</button>
